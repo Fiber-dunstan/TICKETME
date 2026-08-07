186426615b5d34349bc3df6abdd06b3799963b17
Fiber-dunstan <dunstanbanyaa11@gmail.com>
2026-08-07 21:12:27 +0000

    style: fix budgets.tf formatting

--- infrastructure/budgets.tf
+++ infrastructure/budgets.tf
@@ -13,16 +13,16 @@ resource "aws_budgets_budget" "monthly_cost" {
   notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 80
-    threshold_type              = "PERCENTAGE"
-    notification_type           = "ACTUAL"
-    subscriber_email_addresses  = [var.alert_email]
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.alert_email]
   }
 
   notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 100
-    threshold_type              = "PERCENTAGE"
-    notification_type           = "FORECASTED"
-    subscriber_email_addresses  = [var.alert_email]
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.alert_email]
   }
 }
